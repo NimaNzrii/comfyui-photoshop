0ce789d8994e76db6ab5f743f4300a2758333a2d
NimaNzrii <nimanzriart@gmail.com>
2023-12-10 01:09:55 +0330

    v1

---
+++ __init__.py
@@ -0,0 +1,21 @@
+import importlib
+import subprocess
+try:
+    from photoshop import PhotoshopConnection
+except ImportError:
+    subprocess.run(["./python_embeded/python.exe", "-m", "pip", "install", "photoshop-connection"])
+
+node_list = [ 
+    "photoshop"
+]
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+for module_name in node_list:
+    imported_module = importlib.import_module(".{}".format(module_name), __name__)
+
+    NODE_CLASS_MAPPINGS = {**NODE_CLASS_MAPPINGS, **imported_module.NODE_CLASS_MAPPINGS}
+    NODE_DISPLAY_NAME_MAPPINGS = {**NODE_DISPLAY_NAME_MAPPINGS, **imported_module.NODE_DISPLAY_NAME_MAPPINGS}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
