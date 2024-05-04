350a9f0d1c0cd1063cbbf229878c0ac8641b20e4
NimaNzrii <nimanzriart@gmail.com>
2024-05-04 20:15:52 +0330

    Added debugging logs

--- __init__.py
+++ __init__.py
@@ -5,22 +5,33 @@ import folder_paths
 import sys
 
 node_path = os.path.join(folder_paths.get_folder_paths("custom_nodes")[0], "comfyui-photoshop")
-
+if not os.path.exists(node_path):
+    print("✨ Node path does not exist!")
+    
 venv_path = os.path.join(node_path, "venv")
 
 if not os.path.exists(venv_path):
-    print("Installing venv...")
+    print("✨ Installing venv...")
     subprocess.run([sys.executable, '-m', 'virtualenv', venv_path], shell=True)
-    print("Installing requirements...")
+    print("✨ Installing requirements...")
     requirements_path = os.path.join(node_path, 'requirements.txt')
     subprocess.run([os.path.join(venv_path, 'Scripts', 'python.exe'), '-m', 'pip', 'install', '-r', requirements_path], shell=True)
-    print("Installed successfully")
+    print("✨ Installed successfully")
 
-popup_window_path = os.path.join(node_path, 'Backend.py')
+backend_path = os.path.join(node_path, 'Backend.py')
 python_path = os.path.join(venv_path, 'Scripts', 'python.exe')
 
-subprocess.Popen([python_path, popup_window_path])
 
+if not os.path.exists(venv_path):
+    print("✨ venv_path does not exist!")
+
+if not os.path.exists(python_path):
+    print("✨ Python path does not exist!")
+    
+if not os.path.exists(backend_path):
+    print("✨ Backend.py path does not exist!")
+
+subprocess.Popen([python_path, backend_path])
 
 
 try:
