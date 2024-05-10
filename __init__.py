d20a5c20c36de1cea715db73df53ef76e3fd9ad2
NimaNzrii <nimanzriart@gmail.com>
2024-05-10 11:29:07 +0330

    Debugging update

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import folder_paths
 import sys
+
 sys.stdout.reconfigure(encoding='utf-8')
 
 def get_python_executable(venv_path):
@@ -11,69 +12,73 @@ def get_python_executable(venv_path):
     else:
         return os.path.join(venv_path, 'bin', 'python')
 
-node_path = os.path.join(folder_paths.get_folder_paths("custom_nodes")[0], "comfyui-photoshop")
-venv_path = os.path.join(node_path, "venv")
+try:
+    node_path = os.path.join(folder_paths.get_folder_paths("custom_nodes")[0], "comfyui-photoshop")
+    venv_path = os.path.join(node_path, "venv")
 
-if not os.path.exists(node_path):
-    print(f"_PS_ Node path does not exist: {node_path}")
+    if not os.path.exists(node_path):
+        raise FileNotFoundError(f"_PS_ Node path does not exist: {node_path}")
 
-if not os.path.exists(venv_path):
+    if not os.path.exists(venv_path):
+        print("_PS_ Installing venv...")
+        subprocess.run([sys.executable, '-m', 'pip', 'install', 'virtualenv'], check=True)
+        if sys.platform.startswith('win'):
+            subprocess.run([sys.executable, '-m', 'virtualenv', venv_path], shell=True, check=True)
+        else:
+            subprocess.run([sys.executable, '-m', 'venv', venv_path], check=True)
 
-    print("_PS_ Installing venv...")
-    subprocess.run([sys.executable, '-m', 'pip', 'install', 'virtualenv'], check=True)
-    if sys.platform.startswith('win'):
-        subprocess.run([sys.executable, '-m', 'virtualenv', venv_path], shell=True, check=True)
-    else:
-        subprocess.run([sys.executable, '-m', 'venv', venv_path], check=True)
-        
-        
-    python_executable = get_python_executable(venv_path)
-    if not os.path.exists(python_executable):
-        print(f"_PS_ python_path path does not exist")
-    else: 
-        print(f"_PS_ python_path Installed Succedfully")
-    
-    print("_PS_ Installing requirements...")
-    requirements_path = os.path.join(node_path, 'requirements.txt')
-    subprocess.run([python_executable, '-m', 'pip', 'install', 'Pillow'], check=True)
-    subprocess.run([python_executable, '-m', 'pip', 'install', 'asyncio'], check=True)
-    subprocess.run([python_executable, '-m', 'pip', 'install', 'websockets'], check=True)
-    print("_PS_ Installed successfully")
+        python_executable = get_python_executable(venv_path)
+        if not os.path.exists(python_executable):
+            raise FileNotFoundError(f"_PS_ python_path path does not exist")
+        else: 
+            print(f"_PS_ python_path Installed Successfully")
 
-backend_path = os.path.join(node_path, 'Backend.py')
-python_path = get_python_executable(venv_path)
+        print("_PS_ Installing requirements...")
+        requirements_path = os.path.join(node_path, 'requirements.txt')
+        subprocess.run([python_executable, '-m', 'pip', 'install', 'Pillow'], check=True)
+        subprocess.run([python_executable, '-m', 'pip', 'install', 'asyncio'], check=True)
+        subprocess.run([python_executable, '-m', 'pip', 'install', 'websockets'], check=True)
+        print("_PS_ Installed successfully")
 
-print("_PS_ python_path", python_path)
-print("_PS_ backend_path", backend_path)
+    backend_path = os.path.join(node_path, 'Backend.py')
+    python_path = get_python_executable(venv_path)
 
-if not os.path.exists(python_path):
-    print(f"_PS_ python_path path does not exist")
+    print("_PS_ python_path", python_path)
+    print("_PS_ backend_path", backend_path)
 
-if not os.path.exists(backend_path):
-    print(f"_PS_ Backend.py path does not exist")
+    if not os.path.exists(python_path):
+        raise FileNotFoundError(f"_PS_ python_path path does not exist")
 
+    if not os.path.exists(backend_path):
+        raise FileNotFoundError(f"_PS_ Backend.py path does not exist")
 
-default_directory = os.getcwd()
-node_path = os.path.join(folder_paths.get_folder_paths("custom_nodes")[0], "comfyui-photoshop")
+    default_directory = os.getcwd()
+    node_path = os.path.join(folder_paths.get_folder_paths("custom_nodes")[0], "comfyui-photoshop")
 
-print("_PS_ node_path", node_path)
+    print("_PS_ node_path", node_path)
 
-os.chdir(node_path)
-subprocess.Popen([python_path, backend_path])
-os.chdir(default_directory)
+    os.chdir(node_path)
+    subprocess.Popen([python_path, backend_path])
+    os.chdir(default_directory)
 
+    node_list = ["node-Photoshop", "node-Photoshop-noplugin"]
+    NODE_CLASS_MAPPINGS = {}
+    NODE_DISPLAY_NAME_MAPPINGS = {}
 
-node_list = ["node-Photoshop", "node-Photoshop-noplugin"]
-NODE_CLASS_MAPPINGS = {}
-NODE_DISPLAY_NAME_MAPPINGS = {}
+    for module_name in node_list:
+        imported_module = importlib.import_module(".{}".format(module_name), __name__)
+        NODE_CLASS_MAPPINGS = {**NODE_CLASS_MAPPINGS, **imported_module.NODE_CLASS_MAPPINGS}
+        NODE_DISPLAY_NAME_MAPPINGS = {
+            **NODE_DISPLAY_NAME_MAPPINGS,
+            **imported_module.NODE_DISPLAY_NAME_MAPPINGS,
+        }
 
-for module_name in node_list:
-    imported_module = importlib.import_module(".{}".format(module_name), __name__)
-    NODE_CLASS_MAPPINGS = {**NODE_CLASS_MAPPINGS, **imported_module.NODE_CLASS_MAPPINGS}
-    NODE_DISPLAY_NAME_MAPPINGS = {
-        **NODE_DISPLAY_NAME_MAPPINGS,
-        **imported_module.NODE_DISPLAY_NAME_MAPPINGS,
-    }
+    __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+    WEB_DIRECTORY = "js"
 
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
-WEB_DIRECTORY = "js"
+except FileNotFoundError as e:
+    print(str(e))
+except subprocess.CalledProcessError as e:
+    print(f"_PS_ An error occurred while running a subprocess: {e}")
+except Exception as e:
+    print(f"_PS_ An unexpected error occurred: {e}")
